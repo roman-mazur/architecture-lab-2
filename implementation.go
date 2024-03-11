2ca7b5aaf72daa866983baddfd349995dd4c1170
kujo205 <ivan.kuts@itjet.io>
2024-03-12 00:07:14 +0200

    feat: added prefix-to-postfix convertor

--- implementation.go
+++ implementation.go
@@ -1,9 +1,40 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
+// PrefixToPostfix converts prefix notation to postfix notation.
 func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	stack := []string{}
+	tokens := strings.Fields(input)
+
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+		if isOperator(token[0]) {
+			if len(stack) < 2 {
+				return "", errors.New("not enough operands for operator")
+			}
+			operand1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			postfix := operand1 + " " + operand2 + " " + token
+			stack = append(stack, postfix)
+		} else {
+			stack = append(stack, token)
+		}
+	}
+
+	if len(stack) != 1 {
+		return "", errors.New("invalid expression format")
+	}
+
+	return stack[0], nil
 }
+
+func isOperator(ch byte) bool {
+	return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^'
+}
+
